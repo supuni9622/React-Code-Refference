c489aeba0b5e00fd76f96600373b65299d781483
Supuni22 <supunimanamperi@gmail.com>
2020-10-24 22:54:16 +0530

    Did small modification in navigations

--- my-app/src/Naviagtion.js
+++ my-app/src/Naviagtion.js
@@ -4,19 +4,12 @@ import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
 const Naviagtion = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-        <Navbar.Brand href="#home">React-Hooks</Navbar.Brand>
+        <Navbar.Brand href="/">React-Hooks</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <NavDropdown title="useState" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/useState">Example 1</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/useState/hookCounter">Example 2</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">Example 3</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Example 4</NavDropdown.Item>
-            </NavDropdown>
+          <Nav.Link href="/useState">useState</Nav.Link>
+          <Nav.Link href="/useEffect">useEffect</Nav.Link>
             <NavDropdown title="useEffect" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Example 1</NavDropdown.Item>
               <NavDropdown.Divider />
