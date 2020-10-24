95220055227738cb6d0d7d92d892507ddef29b26
Supuni22 <supunimanamperi@gmail.com>
2020-10-24 13:24:09 +0530

    Basic navigations and routings are done

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import Navigation from './Naviagtion'
+import { Switch, Route } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            <h3>This is Home page</h3>
-        </div>
+        <>
+            <Navigation/>
+            <div>
+                <Switch>
+                    <Route name="useState" exact path="/useState" component={"Hi"} />
+                    <Route name="useEffect" exact path="/useEffect" component={"Hi"} />
+                    <Route name="useContext" exact path="/useContext" component={"Hi"} />
+                    <Route name="useReducer" path="/useReducer" component={"Hi"} />
+                    <Route name="useMemo" exact path="/useMemo" component={"Hi"} />
+                    <Route name="useRef" exact path="/useRef" component={"Hi"} />
+                    <Route name="useCallback" exact path="/useCallback" component={"Hi"} />
+                    <Route name="customHooks" exact path="/customHooks" component={"Hi"} />
+                </Switch>
+            </div>
+           
+        </>
     )
 }
 
---
+++ my-app/src/Naviagtion.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
+
+const Naviagtion = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+        <Navbar.Brand href="#home">React-Hooks</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <NavDropdown title="useState" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Example 1</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">Example 2</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Example 3</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Example 4</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="useEffect" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Example 1</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">Example 2</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Example 3</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Example 4</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#features">useContext</Nav.Link>
+            <Nav.Link href="#pricing">useReducer</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#pricing">useMemo</Nav.Link>
+            <Nav.Link href="#pricing">useRef</Nav.Link>
+            <Nav.Link href="#pricing">Custom Hooks</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+}
+
+export default  Naviagtion
