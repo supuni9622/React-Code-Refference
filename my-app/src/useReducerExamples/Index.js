9259462897157827feda1723a4c1da73dfed0fa0
Supuni22 <supunimanamperi@gmail.com>
2020-10-26 22:23:26 +0530

    Introduced useReducer hook and reduce() function

--- my-app/src/useReducerExamples/Index.js
+++ my-app/src/useReducerExamples/Index.js
@@ -1,5 +1,31 @@
 import React from 'react'
 
+/*
+    * useReducer is a hook that is used for state management
+    * It's an alternative to useState
+    * useReducer is a most primitive hook compaired to useState. useState is built using useReducer
+    *
+    * Reduce method in vanila java script takes 2 parameters - reducer function & initial value
+    * reducer() returns a single value 
+    * useReducer() also accepts 2 parameters - currentState, action
+    * useReducer() returns a newState
+    * useReducer() hook returns pair of values - [newState, dispatch]
+    * dispatch method is basically used to specify the action
+    * 
+    * ===========================================
+    * 
+    * useReducer(reducer, initialState)
+    * reducer(currentState, action)
+*/
+
+
+// Let's check reduce() method
+const numbers = [1,2,3,4,5,6]
+const reducer = (accumilator, initialValue) => accumilator + initialValue
+const sum = numbers.reduce(reducer, 4)
+console.log("Testing reduce() in vanila javaScript :" + sum)
+// Answer ==> 25 == 4 + 1 + 2 + 3 + 4 + 5 + 6
+
 const IndexReducer = () => {
     return (
         <div>
