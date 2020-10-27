cc52b42d9ca3c0e063205c59c10f691c5cabd311
Supuni22 <supunimanamperi@gmail.com>
2020-10-27 06:52:03 +0530

    Modified CounterTwo

--- my-app/src/useReducerExamples/components/CounterTwo.js
+++ my-app/src/useReducerExamples/components/CounterTwo.js
@@ -1,16 +1,25 @@
 import React, {useReducer} from 'react'
 
-// Here we use state object instead of direct value
+// State as an object
 const initialState = {
-    firstCounter : 0
+    firstCounter : 0,
+    secondCounter : 10
 }
 
+// Now we have two properties in the state object but changing only one at a time 
+// To get the expected output we have to modify the return statement to merge the state properties - use spread operator
 const reducer = (state, action) => {
     switch(action.type) {
         case 'increment': 
-           return { firstCounter: state.firstCounter + 1}
+           return { ...state, firstCounter: state.firstCounter + action.value} // Action as an object
         case 'decrement' : 
-            return { firstCounter: state.firstCounter -1 }
+            return { ...state, firstCounter: state.firstCounter - action.value }
+
+        // These are for counterTwo
+        case 'increment2': 
+            return { ...state, secondCounter: state.secondCounter + action.value} // Action as an object
+         case 'decrement2' : 
+             return { ...state, secondCounter: state.secondCounter - action.value }
         case 'reset' : 
             return initialState
         default : 
@@ -23,11 +32,37 @@ const CounterTwo = () => {
     const [count, dispatch] = useReducer(reducer, initialState)
 
     return (
-        <div className='p-3 m-3' style={{backgroundColor:'#f9f89f'}}>
-            <h3> Count - {count.firstCounter}</h3>
-            <button className="btn btn-outline-success p-1 m-2" onClick={()=> dispatch( {type: 'increment'})}>Increment</button>
-            <button className="btn btn-outline-warning p-1 m-2" onClick={()=> dispatch({type: 'decrement'})}>Decrement</button>
-            <button className="btn btn-outline-danger p-1 m-2" onClick={()=> dispatch({type: 'reset'})}>Reset</button> 
+        <div className='p-3 m-3' style={{backgroundColor:'#f9f89f', textAlign:'center'}}>
+            <h3> First Count - {count.firstCounter}</h3>
+            <h3> Second Counter - {count.secondCounter} </h3>
+            <div>
+                <button className="btn btn-outline-success p-1 m-2" onClick={()=> dispatch( {type: 'increment', value: 1 })}>
+                    Increment
+                </button>
+                <button className="btn btn-outline-warning p-1 m-2" onClick={()=> dispatch({type: 'decrement', value: 1 })}>
+                    Decrement
+                </button>
+                <button className="btn btn-outline-primary p-1 m-2" onClick={()=> dispatch( {type: 'increment', value: 5 })}>
+                    Increment
+                </button>
+                <button className="btn btn-outline-secondary p-1 m-2" onClick={()=> dispatch({type: 'decrement', value: 5 })}>
+                    Decrement
+                </button>
+            </div>
+
+            {/* For counter two  */}
+            <div>
+                <button className="btn btn-outline-success p-1 m-2" onClick={()=> dispatch( {type: 'increment2', value: 1 })}>
+                    Increment Counter 2
+                </button>
+                <button className="btn btn-outline-warning p-1 m-2" onClick={()=> dispatch({type: 'decrement2', value: 1 })}>
+                    Decrement Counter 2
+                </button>
+            </div>
+
+            <button className="btn btn-outline-danger p-1 m-2" onClick={()=> dispatch({type: 'reset', value: 1 })}>
+                Reset
+            </button> 
         </div>
     )
 }
@@ -36,5 +71,6 @@ export default CounterTwo
 
 /*
     What is the advantage of using this pattern?
-        * When we have more than one property
+        * When we have action as an object we can use additional data (properties)
+        * When we use state as an object, we are able to keep track of multiple state variables
 */
