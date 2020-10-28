25a0b57df51e269216e8a8892572ae35d76a00db
Supuni22 <supunimanamperi@gmail.com>
2020-10-28 23:09:14 +0530

    Further improved performence with useCallback

--- my-app/src/useCallbackExamples/IndexCallback.js
+++ my-app/src/useCallbackExamples/IndexCallback.js
@@ -11,6 +11,17 @@ import ParentComponent from './components/ParentComponent'
         *  React.memo ------ Higher order component that will prevent a functional component from being re-rendered
             if it's prop or states do not change
 
+        * useCallback hook ---------------
+        *       What ?
+        *       useCallback is a hook that will return a memorized version of the callback function that only 
+                changes if one of the dependencies has changed. 
+        *       Why ?
+        *       It is useful when passing callbacks to optimse child components that rely on reference equality to 
+        *       prevent unneccesary renders.
+        *       How ?
+        *       1. Import from react
+        *       2. call useCallback which accepts callback function as the first parameter and array of dependencies as second parameter
+
 */
 
 const IndexCallback = () => {
