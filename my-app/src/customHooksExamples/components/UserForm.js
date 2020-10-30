432f539deacd6b524d0d04595ee695ea65261457
Supuni22 <supunimanamperi@gmail.com>
2020-10-30 10:21:02 +0530

    Modified with useInput hook

--- my-app/src/customHooksExamples/components/UserForm.js
+++ my-app/src/customHooksExamples/components/UserForm.js
@@ -1,24 +1,28 @@
 import React, {useState} from 'react'
+import useInput from '../hooks/useInput'
 
 const UserForm = () => {
 
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
+    // Using useInput custom hook
+    const [firstName,bindFirstName, resetFirstName] = useInput('')
+    const [lastName,bindLastName, resetLastName] = useInput('')
 
     const submitHandler = e => {
         e.preventDefault()
         alert(`Hello ${firstName} ${lastName}`)
+        resetFirstName()
+        resetLastName()
     }
     return (
         <div className='m-3 p-3' style={{backgroundColor:'#cccccc'}}>
             <form onSubmit={submitHandler} className='p-3 m-2'>
                 <div>
                     <label>First Name</label>
-                    <input value={firstName} onChange = {e => setFirstName(e.target.value)} type='text'/> 
+                    <input {...bindFirstName} type='text'/> 
                 </div>
                 <div>
                     <label>Second Name</label>
-                    <input value={lastName} onChange = {e => setLastName(e.target.value)} type='text'/> 
+                    <input {...bindLastName} type='text'/> 
                 </div>
                 <button >Submit</button>
             </form>
--- my-app/src/customHooksExamples/hooks/useInput.js
+++ my-app/src/customHooksExamples/hooks/useInput.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 
-const useInput = () => {
+const useInput = (initialValue) => {
     const [value, setValue] = useState(initialValue)
     const reset = () => {
         setValue(initialValue)
