12246a15bd2a7131f5cd4b3939ef82192a2f1012
Supuni22 <supunimanamperi@gmail.com>
2022-05-15 18:13:42 +0530

    Updates

---
+++ precache-manifest.55eb7eb7a2835c8af80e4fdd9f817c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da14a5d3347fca8d950d90edb246731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7641e0700080b0fcc906",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a13a69c9378c3150f82d",
+    "url": "/static/js/2.70d604b2.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.70d604b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7641e0700080b0fcc906",
+    "url": "/static/js/main.8f4ff606.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Code-Refference/precache-manifest.0aa45cf6d09044d987248e4773d3ad42.js"
+  "/precache-manifest.55eb7eb7a2835c8af80e4fdd9f817c33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Code-Refference/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
